a61b071462016568c7226a87339c6ae48ff5fa29
Radosław <radek.barylak@gmail.com>
2025-03-20 19:56:04 +0100

    Created Image Model and migrations

--- openai_image_generator/image_generator/forms.py
+++ openai_image_generator/image_generator/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ImageForm(forms.Form):
-    prompt = forms.CharField(label="prompt", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    prompt = forms.CharField(label="prompt", max_length=4000, widget=forms.TextInput(attrs={'class': 'form-control'}))
     image_size = forms.ChoiceField(
       label="Size",
 			choices=[("1024x1024", "1024x1024"), ("1792x1024", "1792x1024"), ("1024x1792", "1024x1792")],
--- openai_image_generator/image_generator/models.py
+++ openai_image_generator/image_generator/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+	image_id = models.UUIDField(primary_key=True)
+	prompt = models.CharField(max_length=4000)
+	created_at = models.DateField(auto_now=True)
